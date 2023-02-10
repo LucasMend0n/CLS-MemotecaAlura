284ede71971b5fba3efdf2a4dca4fc28fc89c330
Lucas Mendonça <lucasjr413@gmail.com>
2023-02-10 14:55:11 -0300

    Curso2-Aula04-ImplementandoCampoDeBusca

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -12,20 +12,21 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamentos : Pensamento[] = [];
   paginaAtual : number = 1;
   haMaisPensamentos: boolean = true;
+  filtro : string = '';
 
 
   constructor(private service : PensamentoService) { }
 
   ngOnInit(): void {
 
-    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos;
     })
 
   }
 
   carregarMaisPensamentos(){
-    this.service.listar(this.paginaAtual++)
+    this.service.listar(this.paginaAtual++, this.filtro)
     .subscribe(listaPensamentos => {
       this.listaPensamentos.push(...listaPensamentos);
       if(!listaPensamentos.length){
@@ -34,5 +35,14 @@ export class ListarPensamentoComponent implements OnInit {
     })
   }
 
+  pesquisarPensamentos(){
+
+    this.haMaisPensamentos = true;
+    this.paginaAtual = 1;
+    this.service.listar(this.paginaAtual, this.filtro)
+    .subscribe(listaPensamentos => {
+      this.listaPensamentos = listaPensamentos
+    })
+  }
 
 }
